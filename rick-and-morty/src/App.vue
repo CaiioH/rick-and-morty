610ea9771f2c86be95c4ddad3f6ba0369491a370
CaiioH <caio.rhenrique@hotmail.com>
2024-07-12 18:03:16 -0300

    update menu

---
+++ rick-and-morty/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Menu />
+    <!-- O restante do seu código vai aqui -->
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
---
+++ rick-and-morty/src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+      <img src="../assets/svgs/path_to_your_logo.png" alt="Logo">
+      <ul>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MainMenu'
+  }
+  </script>
+  
+  <style scoped>
+  /* Adicione aqui o CSS para o seu componente */
+  </style>
